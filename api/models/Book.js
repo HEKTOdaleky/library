fe42ea5df46eaa96c4c85e59e96f9049bd421f82
Saver2003 <saver2003@gmail.com>
2018-06-22 18:44:08 +0600

    #4 Создал модель Book

---
+++ api/models/Book.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  year:{
+    type: Number,
+    required: true
+  },
+  registerDate: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'onStock',
+    enum: ['onStock', 'issued', 'decommissioned']
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
