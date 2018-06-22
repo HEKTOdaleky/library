db35a8b58bc73ac2ca698a580fca24df9449d6f8
Vladimir Minakov <minakov.vladimir2009@gmail.com>
2018-06-22 20:27:47 +0600

    #5 - В роут books добавлен роутер GET: '/categories/:id'

--- api/app/books.js
+++ api/app/books.js
@@ -30,7 +30,20 @@ const createRouter = () => {
     } catch (error) {
       return res.status(400).send({error});
     }
+  });
 
+  router.get('/categories/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      const book = await Book.find({category: id});
+      if (book) {
+        res.send(book);
+      }
+      else res.sendStatus(404);
+    } catch (error) {
+      return res.status(500).send({error});
+    }
   });
 
 
