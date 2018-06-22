2d65aab12d4536b96f432e627413bfbb8936dd91
Andrey Detenkov <a.detenkov@gmail.com>
2018-06-22 18:04:56 +0600

    #7 создан server.js. сервер работает на 8000 порту.
    подключена БД к проекту - library, порт- 27017

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const app = express();
+
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(config.db.url + "/" + config.db.name);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Mongoose connected!");
+
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+});
