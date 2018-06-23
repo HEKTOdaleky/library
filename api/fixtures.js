ea7218773f01153f74c33b7fde69110f8ac9fcca
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-06-23 09:54:27 +0600

    #6 Добавлены фикстуры для модели читателя

--- api/fixtures.js
+++ api/fixtures.js
@@ -3,48 +3,73 @@ const config = require('./config');
 
 const Category = require('./models/Category');
 const Book = require('./models/Book');
+const Reader = require('./models/Reader');
+
 
 mongoose.connect(config.db.url + '/' + config.db.name);
 
 const db = mongoose.connection;
 
 db.once('open', async () => {
-  try {
-    await db.dropCollection('categories');
-    await db.dropCollection('books');
-  } catch (e) {
-    console.log('Collections were not present, skipping drop...');
-  }
+    try {
+        await db.dropCollection('categories');
+        await db.dropCollection('books');
+        await db.dropCollection('readers');
 
-  const [textbook, reference] = await Category.create({
-    title: 'Учебники',
-    description: 'Учебники'
-  }, {
-    title: 'Справочники',
-    description: 'Справочники'
-  });
+    } catch (e) {
+        console.log('Collections were not present, skipping drop...');
+    }
 
-  await Book.create({
-    title: 'Таблица Брадиса',
-    author: 'Брадис',
-    year: 1965,
-    categoryId: reference._id
-  }, {
-    title: 'Учебник по математике 6 класс',
-    author: 'В. Пупкин',
-    year: 1977,
-    categoryId: textbook._id
-  }, {
-    title: 'Учебник по астрономии 10 класс',
-    author: 'John Doe',
-    year: 1998,
-    categoryId: textbook._id
-  }, {
-    title: 'Физика. Справочное руководство: для поступающих в ВУЗы',
-    author: 'Яворский Б.М.',
-    year: 1999,
-    categoryId: reference._id
-  });
+    const [textbook, reference] = await Category.create({
+        title: 'Учебники',
+        description: 'Учебники'
+    }, {
+        title: 'Справочники',
+        description: 'Справочники'
+    });
 
-  db.close();
+    await Book.create({
+        title: 'Таблица Брадиса',
+        author: 'Брадис',
+        year: 1965,
+        categoryId: reference._id
+    }, {
+        title: 'Учебник по математике 6 класс',
+        author: 'В. Пупкин',
+        year: 1977,
+        categoryId: textbook._id
+    }, {
+        title: 'Учебник по астрономии 10 класс',
+        author: 'John Doe',
+        year: 1998,
+        categoryId: textbook._id
+    }, {
+        title: 'Физика. Справочное руководство: для поступающих в ВУЗы',
+        author: 'Яворский Б.М.',
+        year: 1999,
+        categoryId: reference._id
+    });
+
+    const [Ivan, Igor, Mikhail] = await Reader.create([
+        {
+            firstName: "Ivan",
+            lastName: "Ivanov",
+            document: "sudy",
+            group: "js1"
+        },
+        {
+            firstName: "Igor",
+            lastName: "Ignatov",
+            document: "sudy",
+            group: "js2"
+        },
+        {
+            firstName: "Mikhail",
+            lastName: "Petrov",
+            document: "sudy",
+            group: "js2"
+        }
+    ]);
+
+    db.close();
 });
