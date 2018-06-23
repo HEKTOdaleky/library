2f515f2db7bd854c0caadd40da2abf927497270e
Saver2003 <saver2003@gmail.com>
2018-06-23 17:20:07 +0600

    #21 Добавил модель Status

--- api/app/reader.js
+++ api/app/reader.js
@@ -8,56 +8,56 @@ const auth = require('../middleware/auth');
 
 
 const createRouter = () => {
-    const router = express.Router();
+  const router = express.Router();
 
-    router.get('/', auth, (req, res) => {
-        Reader.find().then(results => {
-            res.send(results)
-        })
-            .catch(() => res.sendStatus(500));
+  router.get('/', auth, (req, res) => {
+    Reader.find().then(results => {
+      res.send(results)
+    })
+      .catch(() => res.sendStatus(500));
 
-    });
-
-
-    router.post('/', auth, async (req, res) => {
-        let data = {};
-        data.firstName = req.body.firstName;
-        data.lastName = req.body.lastName;
-        data.document = req.body.document;
-        data.group = req.body.group;
-        const reader = new Reader(data);
-
-        await reader.save();
+  });
 
 
-        res.send(reader);
-    });
-    router.delete('/:id', auth, (req, res) => {
-        const id = req.params.id;
-        Reader.deleteOne({_id: id}).then(results => {
-            res.send(results)
-        })
-            .catch(() => res.sendStatus(500));
-    });
-    router.put('/:id', auth, async (req, res) => {
-        const id = req.params.id;
-        let reader;
-        try {
-            reader = await Reader.findOne({_id: id});
-        }
-        catch (error) {
-            res.status(500).send(error);
-        }
-        ;
+  router.post('/', auth, async (req, res) => {
+    let data = {};
+    data.firstName = req.body.firstName;
+    data.lastName = req.body.lastName;
+    data.document = req.body.document;
+    data.group = req.body.group;
+    const reader = new Reader(data);
 
-        for (i in req.body) {
-            reader[i] ? reader[i] = req.body[i] : null
-        }
-        await reader.save();
+    await reader.save();
 
-        res.send(reader);
-    });
 
-    return router;
+    res.send(reader);
+  });
+  router.delete('/:id', auth, (req, res) => {
+    const id = req.params.id;
+    Reader.deleteOne({_id: id}).then(results => {
+      res.send(results)
+    })
+      .catch(() => res.sendStatus(500));
+  });
+  router.put('/:id', auth, async (req, res) => {
+    const id = req.params.id;
+    let reader;
+    try {
+      reader = await Reader.findOne({_id: id});
+    }
+    catch (error) {
+      res.status(500).send(error);
+    }
+    ;
+
+    for (i in req.body) {
+      reader[i] ? reader[i] = req.body[i] : null
+    }
+    await reader.save();
+
+    res.send(reader);
+  });
+
+  return router;
 };
 module.exports = createRouter;
---
+++ api/models/Status.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StatusSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+const Status = mongoose.model('Status', StatusSchema);
+
+model.exports = Status;
