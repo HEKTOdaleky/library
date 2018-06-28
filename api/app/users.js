b7a5ae85757f3c3aa6a6b6ccf5d3287a25477f05
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-06-28 19:51:33 +0600

    #44 Ичправлена проблема с логином

--- api/app/users.js
+++ api/app/users.js
@@ -2,55 +2,58 @@ const express = require('express');
 const User = require('../models/User');
 
 const createRouter = () => {
-  const router = express.Router();
+    const router = express.Router();
 
-  router.post('/', (req, res) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
+    router.post('/', (req, res) => {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        user.save()
+            .then(user => res.send(user))
+            .catch(error => res.status(400).send(error))
     });
 
-    user.save()
-      .then(user => res.send(user))
-      .catch(error => res.status(400).send(error))
-  });
+    router.post('/sessions', async (req, res) => {
+        const user = await User.findOne({username: req.body.username});
 
-  router.post('/sessions', async (req, res) => {
-    const user = await User.findOne({username: req.body.username});
+        if (!user) {
+            return res.status(400).send({error: 'Имя пользователя или пароль неправильные!'});
+        }
 
-    if (!user) {
-      return res.status(400).send({error: 'Имя пользователя или пароль неправильные!'});
-    }
+        const isMatch = await user.checkPassword(req.body.password);
 
-    const isMatch = await user.checkPassword(req.body.password);
+        if (!isMatch) {
+            return res.status(400).send({error: 'Имя пользователя или пароль неправильные!'});
+        }
 
-    if (!isMatch) {
-      return res.status(400).send({error: 'Имя пользователя или пароль неправильные!'});
-    }
-
-    const token = user.generateToken();
-
-    return res.send({message: 'Пользователь и пароль правильные!', user, token});
-  });
+        const token = user.generateToken();
+        user.token = token;
+        await user.save();
 
 
-  router.delete('/sessions', async (req, res) => {
-    const token = req.get('Token');
-    const success = {message: 'Вы успешно вышли из приложения!'};
+        return res.send({message: 'Пользователь и пароль правильные!', user, token});
+    });
 
-    if (!token) return res.send(success);
 
-    const user = await User.findOne({token});
+    router.delete('/sessions', async (req, res) => {
+        const token = req.get('Token');
+        const success = {message: 'Вы успешно вышли из приложения!'};
 
-    if (!user) return res.send(success);
+        if (!token) return res.send(success);
 
-    user.generateToken();
-    await user.save();
+        const user = await User.findOne({token});
 
-    return res.send(success);
-  });
+        if (!user) return res.send(success);
 
-  return router;
+        user.generateToken();
+        await user.save();
+
+        return res.send(success);
+    });
+
+    return router;
 };
 
 module.exports = createRouter;
