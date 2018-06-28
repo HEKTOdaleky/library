441828294c6c6cd9af5df8b467b9298360fd011d
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-06-28 19:11:11 +0600

    #42 Создан компонент для книги

---
+++ client/src/containers/AddBook/AddBook.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+class AddBook extends Component {
+
+};
+
+
+const mapStateToProps = state => {
+    return {};
+};
+
+const mapDispatchToProps = dispatch => {
+    return {};
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddBook);
