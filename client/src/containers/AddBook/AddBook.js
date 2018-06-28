e49d2959a0bc62aad45e28c9bba366ea3b1e99e6
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-06-29 00:29:13 +0600

    #42 Добавлены поля для заполнения информации о книги, добавлена библиотека по форматированию дат

--- client/src/containers/AddBook/AddBook.js
+++ client/src/containers/AddBook/AddBook.js
@@ -3,6 +3,9 @@ import {connect} from "react-redux";
 import {getLanguage} from "../../store/actions/languages";
 import {getStatus} from "../../store/actions/status";
 import {getCategories} from "../../store/actions/categories";
+import {ControlLabel, FormControl, FormGroup} from "react-bootstrap";
+import dateFormat from 'dateformat';
+
 
 class AddBook extends Component {
     componentDidMount() {
@@ -11,9 +14,57 @@ class AddBook extends Component {
         this.props.getCategories();
     }
 
+    state = {
+        title: "",
+        author: "",
+        date: new Date().getFullYear(),
+        registerDate:dateFormat(new Date(), "yyyy-mm-dd")
+    };
+    onChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    getValidationState() {
+        if (this.state.date > 1800 && this.state.date <= new Date().getFullYear()) return 'success';
+        else return 'error';
+        return null;
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <FormGroup validationState={this.getValidationState()}>
+                <ControlLabel>Добавить новую книгу</ControlLabel>
+                <FormControl
+                    type="text"
+                    name="title"
+                    value={this.state.title}
+                    placeholder="Название книги"
+                    onChange={this.onChangeHandler}
+                />
+                <FormControl
+                    type="text"
+                    name="author"
+                    placeholder="Автор"
+                    value={this.state.author}
+                    onChange={this.onChangeHandler}
+                />
+                <FormControl
+                    type="number"
+                    name="date"
+                    placeholder="Год издания"
+                    value={this.state.date}
+                    onChange={this.onChangeHandler}
+                />
+                <FormControl
+                    type="date"
+                    name="registerDate"
+                    placeholder="Дата регистрации книги"
+                    value={this.state.registerDate}
+                    onChange={this.onChangeHandler}
+                />
+            </FormGroup>
         )
     }
 
