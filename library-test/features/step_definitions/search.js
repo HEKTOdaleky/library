4e72499536796198ed65918eadaf0cdbd8a888f7
Andrei Detenkov <a.detenkov@gmail.com>
2018-06-30 17:58:49 +0600

    #51 добавлен тест для неуспешного поиска
     Сценарий: Неуспешный поиск книги
      ✔ Допустим я захожу на страницу поиска
      ✔ Если я ввожу в поле ввода слово "йцукен"
      ✔ Когда я нажимаю на кнопку "Поиск"
      ✔ То я вижу всплывающее сообщение что по запросу ничего не найдено

--- library-test/features/step_definitions/search.js
+++ library-test/features/step_definitions/search.js
@@ -3,6 +3,7 @@ module.exports = function() {
   this.Given(/^я захожу на страницу поиска$/, function () {
     const link = browser.element('.navbar-brand');
     link.click();
+    browser.url('http://localhost:3000');
   });
 
   this.When(/^я ввожу в поле ввода слово "([^"]*)"$/, function (value) {
@@ -19,6 +20,11 @@ module.exports = function() {
     return browser.element('.list-group');
   });
 
+  this.Then(/^я вижу всплывающее сообщение что по запросу ничего не найдено$/, function () {
 
+    browser.waitForText('.message', 5000);
+    const text = browser.element('.message').getText();
+    return expect(text).toBe('По вашему запросу ничего не найдено');
+  });
 
 };
