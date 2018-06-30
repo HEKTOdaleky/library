3ca54c6e31ac9dea0b75c3f8140e7e087d2e7b47
Vladimir Minakov <minakov.vladimir2009@gmail.com>
2018-06-30 15:05:35 +0600

    #49 - Добавлен reducer LANGUAGE_POST_DATA_SUCCESS, LANGUAGE_POST_DATA_FAILURE

--- client/src/store/reducers/languages.js
+++ client/src/store/reducers/languages.js
@@ -1,13 +1,21 @@
-import {GET_LANGUAGES_SUCCESS} from "../actions/actionTypes";
+import {
+  GET_LANGUAGES_SUCCESS,
+  LANGUAGE_POST_DATA_FAILURE, LANGUAGE_POST_DATA_SUCCESS
+} from "../actions/actionTypes";
 
 const initialState = {
-    languages: []
+    languages: [],
+    languageError: null
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_LANGUAGES_SUCCESS:
             return {...state, languages: action.lang};
+      case LANGUAGE_POST_DATA_SUCCESS:
+        return {...state, languageError: null};
+      case LANGUAGE_POST_DATA_FAILURE:
+        return {...state, languageError: action.languageError};
         default:
             return state;
     }
