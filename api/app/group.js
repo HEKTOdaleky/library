ff79e5f0d29632756c6ce7d65dacad06f0df2b14
Aleksei Turkin <a.turkin82@gmail.com>
2018-06-30 17:15:54 +0600

    Добавлена возможность создания группы студентов (сервер/клиент)

--- api/app/group.js
+++ api/app/group.js
@@ -4,40 +4,39 @@ const auth = require('../middleware/auth');
 const Reader = require('../models/Reader');
 const permit = require('../middleware/permit');
 
-
-
 const router = express.Router();
 
 const createRouter = () => {
+  router.get('/', [auth, permit('admin','employee')], (req, res) => {
+    Group.find().then(results => {
+      res.send(results)
+    }).catch(() => res.sendStatus(500));
+  });
 
-    router.get('/', [auth, permit('admin','employee')], (req, res) => {
-        Group.find().then(results => {
-            res.send(results)
-        }).catch(() => res.sendStatus(500));
+  router.delete('/:id', [auth, permit('admin')], async (req, res) => {
+    const id = req.params.id;
+    const currentGroup = await Reader.findOne({groupId: id});
+    if (currentGroup)
+      res.sendStatus(400).send({message: "The group is not empty"});
+    await Group.deleteOne({_id: id});
+    res.send({message: "Success"});
+  });
+
+  router.post('/', [auth, permit('admin')], (req, res) => {
+    if (req.body.name === '') {
+      return res.status(400).send({message: 'Поле не должно быть пустым!'});
+    }
+
+    const newGroup = new Group({name: req.body.name});
+
+    newGroup.save().then(() => {
+      res.send(newGroup);
+    }).catch(() => {
+      res.status(400).send({message: 'Ошибка! Не удалось добавить группу'});
     });
+  });
 
-    router.delete('/:id', [auth, permit('admin')], async (req, res) => {
-        const id = req.params.id;
-        const currentGroup = await Reader.findOne({groupId: id});
-        console.log(currentGroup);
-        if (currentGroup)
-            res.sendStatus(400).send({message: "The group is not empty"});
-        await Group.deleteOne({_id: id});
-        res.send({message: "Success"});
-
-    });
-
-    router.post('/', [auth, permit('admin')], (req, res) => {
-        const newGroup = new Group({name: req.body.groupName});
-        console.log(newGroup);
-        newGroup.save().then(response => {
-            res.send(newGroup);
-        }, error => {
-            res.sendStatus(400).send(error);
-        });
-    });
-
-    return router;
+  return router;
 };
 
 module.exports = createRouter;
