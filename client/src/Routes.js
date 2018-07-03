a70592f22c12283deb93edc2ded013dc740174ca
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-07-03 16:34:28 +0600

    #53 Раскомичен роут для регистрации пользовотеля

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Redirect, Route, Switch, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
+import {Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
 
 import Login from "./containers/Login/Login";
 import Library from "./containers/Library/Library";
@@ -12,34 +12,41 @@ import AddLanguage from "./containers/AddForms/AddLanguage/AddLanguage";
 import AddStatus from "./containers/AddForms/AddStatus/AddStatus";
 import AddReader from "./containers/AddForms/AddReader/AddReader";
 import AddCategory from "./containers/AddForms/AddCategory/AddCategory";
+import NewUser from "./containers/NewUser/NewUser";
 
-const ProtectedRoute = ({ isAllowed, ...props }) =>
-  isAllowed ? <Route {...props} /> : <Redirect to="/login" />;
+const ProtectedRoute = ({isAllowed, ...props}) =>
+    isAllowed ? <Route {...props} /> : <Redirect to="/login"/>;
 
-const Routes = ({ user }) => {
-  return (
-    <Switch>
-      <Route path="/" exact component={Library} />
-      <ProtectedRoute isAllowed={user && user.role === "librarian"} path="/librarian" exact component={Librarian} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/admin" exact component={Admin} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-book" exact component={AddBook} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-group" exact component={AddGroup} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-language" exact component={AddLanguage} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-status" exact component={AddStatus} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-reader" exact component={AddReader} />
-      <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-category" exact component={AddCategory} />
-      <Route path="/login" exact component={Login} />
-      <Route
-        render={() => <h1 style={{ textAlign: "center" }}>Page not found</h1>}
-      />
-    </Switch>
-  );
+const Routes = ({user}) => {
+    return (
+        <Switch>
+            <Route path="/" exact component={Library}/>
+            <ProtectedRoute isAllowed={user && user.role === "librarian"} path="/librarian" exact
+                            component={Librarian}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/admin" exact component={Admin}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-book" exact component={AddBook}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-group" exact component={AddGroup}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-language" exact
+                            component={AddLanguage}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-status" exact component={AddStatus}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-reader" exact component={AddReader}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/add-category" exact
+                            component={AddCategory}/>
+            <ProtectedRoute isAllowed={user && user.role === "admin"} path="/create-new-user" exact
+                            component={NewUser}/>
+
+            <Route path="/login" exact component={Login}/>
+            <Route
+                render={() => <h1 style={{textAlign: "center"}}>Page not found</h1>}
+            />
+        </Switch>
+    );
 };
 
 const mapStateToProps = state => {
-  return {
-    user: state.users.user
-  };
+    return {
+        user: state.users.user
+    };
 };
 
 export default withRouter(connect(mapStateToProps)(Routes));
