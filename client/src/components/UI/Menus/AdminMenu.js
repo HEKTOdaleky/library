43528e784b6170cfe3d805bcad209122ec61fbda
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-07-06 19:35:51 +0600

    #58 Добавлен личный кабинет пользователя

--- client/src/components/UI/Menus/AdminMenu.js
+++ client/src/components/UI/Menus/AdminMenu.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Nav} from "react-bootstrap";
 import AddAdminMenu from "./AddAdminMenu/AddAdminMenu";
 import DeleteAdminMenu from "./DeleteAdminMenu/DeleteAdminMenu";
+import PersonalAdminMenu from "./PersonaAdminMenu/PersonalAdminMenu";
 
 class AdminMenu extends Component {
 
-  render() {
-    return (
-      <Nav pullLeft>
-        <AddAdminMenu/>
-        <DeleteAdminMenu/>
-      </Nav>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Nav pullLeft>
+                    <AddAdminMenu/>
+                    <DeleteAdminMenu/>
+
+                </Nav>
+                <Nav pullRight>
+                    <PersonalAdminMenu/></Nav>
+            </Fragment>
+        );
+    }
 }
 
 export default AdminMenu;
