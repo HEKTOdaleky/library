5fca97c517927b536ea0f2a27652cf0fed3ac3be
Andrei Detenkov <a.detenkov@gmail.com>
2018-07-08 20:53:12 +0600

    разрешение конфликтов при git merge

--- client/src/store/reducers/readers.js
+++ client/src/store/reducers/readers.js
@@ -1,39 +1,42 @@
 import {
   ADD_NEW_READER_FAILURE,
   ADD_NEW_READER_SUCCESS,
-  GET_READERS_FOR_REMOVE_FAILURE, GET_READERS_FOR_REMOVE_SUCCESS, SEND_READERS_FAILURE
-  ADD_NEW_READER_FAILURE, ADD_NEW_READER_SUCCESS, EDIT_READER_FAILURE,
-  EDIT_READER_SUCCESS, GET_READER_BY_PIN_FAILURE, GET_READER_BY_PIN_SUCCESS
+  GET_READERS_FOR_REMOVE_FAILURE,
+  GET_READERS_FOR_REMOVE_SUCCESS,
+  SEND_READERS_FAILURE,
+  EDIT_READER_FAILURE,
+  EDIT_READER_SUCCESS,
+  GET_READER_BY_PIN_FAILURE,
+  GET_READER_BY_PIN_SUCCESS
 } from "../actions/actionTypes";
 
 const initialState = {
   error: null,
   newReader: null,
-  readers: []
-  newReader: null,
+  readers: [],
   findingReader: null
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NEW_READER_SUCCESS:
-      return {...state, newReader: action.data, error: null};
+      return { ...state, newReader: action.data, error: null };
     case ADD_NEW_READER_FAILURE:
-      return {...state, error: action.error};
+      return { ...state, error: action.error };
     case EDIT_READER_SUCCESS:
-      return {...state, newReader: action.data, error: null};
+      return { ...state, newReader: action.data, error: null };
     case EDIT_READER_FAILURE:
-      return {...state, error: action.error};
+      return { ...state, error: action.error };
     case GET_READER_BY_PIN_SUCCESS:
-      return {...state, findingReader: action.data, error: null};
+      return { ...state, findingReader: action.data, error: null };
     case GET_READER_BY_PIN_FAILURE:
-      return {...state, findingReader: null, error: action.error};
+      return { ...state, findingReader: null, error: action.error };
     case GET_READERS_FOR_REMOVE_SUCCESS:
-      return {...state, readers: action.data};
+      return { ...state, readers: action.data };
     case GET_READERS_FOR_REMOVE_FAILURE:
-      return {...state, error: action.error};
+      return { ...state, error: action.error };
     case SEND_READERS_FAILURE:
-      return {...state, error: action.error};
+      return { ...state, error: action.error };
     default:
       return state;
   }
