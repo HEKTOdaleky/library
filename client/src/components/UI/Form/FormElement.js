0fd064efc25072354f8e52eccce9eb9dd7265086
Maksim Gachevskiy <hektodaleky@gmail.com>
2018-07-17 19:42:03 +0600

    Добавлен props класса для FormElement

--- client/src/components/UI/Form/FormElement.js
+++ client/src/components/UI/Form/FormElement.js
@@ -3,66 +3,68 @@ import PropTypes from 'prop-types';
 import {Col, ControlLabel, FormControl, FormGroup, HelpBlock} from "react-bootstrap";
 
 const FormElement = props => {
-  let componentClass,
-    formControlChildren;
+    let componentClass,
+        formControlChildren;
 
-  if (props.type === 'textarea') {
-    componentClass = 'textarea';
-  }
+    if (props.type === 'textarea') {
+        componentClass = 'textarea';
+    }
 
-  if (props.type === 'select') {
-    componentClass = 'select';
-    // props.options.unshift({id: '', title:  props.title + ' ...'});
+    if (props.type === 'select') {
+        componentClass = 'select';
+        // props.options.unshift({id: '', title:  props.title + ' ...'});
 
-    formControlChildren = props.options.map(element => (
-      <option key={element.id} value={element.id}>{element.title}</option>
-    ))
-  }
+        formControlChildren = props.options.map(element => (
+            <option key={element.id} value={element.id}>{element.title}</option>
+        ))
+    }
 
-  return (
-    <FormGroup
-      bsSize={props.size}
-      controlId={props.propertyName}
-      validationState={props.error && 'error'}
-    >
-      <Col componentClass={ControlLabel} sm={props.titleLength || 2}>
-        {props.title}
-      </Col>
-      <Col sm={props.inputLength || 10}>
-        <FormControl
-          type={props.type}
-          componentClass={componentClass}
-          required={props.required}
-          placeholder={props.placeholder}
-          name={props.propertyName}
-          value={props.value}
-          onChange={props.changeHandler}
-          autoComplete={props.autoComplete}
+    return (
+        <FormGroup
+            bsSize={props.size}
+            controlId={props.propertyName}
+            validationState={props.error && 'error'}
         >
-          {formControlChildren}
-        </FormControl>
-        {props.error &&
-        <HelpBlock>{props.error}</HelpBlock>
-        }
-      </Col>
-    </FormGroup>
-  );
+            <Col componentClass={ControlLabel} sm={props.titleLength || 2}>
+                {props.title}
+            </Col>
+            <Col sm={props.inputLength || 10}>
+                <FormControl
+                    className={props.className}
+                    type={props.type}
+                    componentClass={componentClass}
+                    required={props.required}
+                    placeholder={props.placeholder}
+                    name={props.propertyName}
+                    value={props.value}
+                    onChange={props.changeHandler}
+                    autoComplete={props.autoComplete}
+                >
+                    {formControlChildren}
+                </FormControl>
+                {props.error &&
+                <HelpBlock>{props.error}</HelpBlock>
+                }
+            </Col>
+        </FormGroup>
+    );
 };
 
 FormElement.propTypes = {
-  propertyName: PropTypes.string.isRequired,
-  error: PropTypes.string,
-  size: PropTypes.string,
-  title: PropTypes.string,
-  type: PropTypes.string.isRequired,
-  required: PropTypes.bool,
-  placeholder: PropTypes.string,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
-  changeHandler: PropTypes.func.isRequired,
-  autoComplete: PropTypes.string,
-  options: PropTypes.arrayOf(PropTypes.object),
-  inputLength: PropTypes.number,
-  titleLength: PropTypes.number,
+    propertyName: PropTypes.string.isRequired,
+    error: PropTypes.string,
+    size: PropTypes.string,
+    title: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    required: PropTypes.bool,
+    placeholder: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
+    changeHandler: PropTypes.func.isRequired,
+    autoComplete: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.object),
+    inputLength: PropTypes.number,
+    titleLength: PropTypes.number,
+    className: PropTypes.string
 };
 
 export default FormElement;
