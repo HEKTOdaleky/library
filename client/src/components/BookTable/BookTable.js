0bb2943bb9d41d13b228f371bcb31186c3a02eed
Aleksander Gareev <saver2003@gmail.com>
2018-07-23 20:34:06 +0600

    #91 Функционал таблицы полностью работает

--- client/src/components/BookTable/BookTable.js
+++ client/src/components/BookTable/BookTable.js
@@ -1,27 +1,39 @@
 import React, {Component} from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import moment from "moment/moment";
 
 class BookTable extends Component {
 
   state = {
+    books: [],
     sortName: undefined,
     sortOrder: undefined
   };
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.book) {
+
+      const books = nextProps.book.map(book => {
+        const registerDate = moment(book.registerDate).format("DD-MM-YYYY h:mm");
+        return {
+          ...book,
+          language: book.language.title,
+          categoryId: book.categoryId.title,
+          registerDate
+        }
+      });
+
+      return {books: books};
+    }
+
+    return {books: []};
+  }
+
   onSortChange = (sortName, sortOrder) => {
-    // console.info('onSortChange', arguments);
     this.setState({
       sortName,
       sortOrder
     });
-  };
-
-  languageFormat = (cell, row) => {
-    return row.language.title;
-  };
-
-  categoryFormat = (cell, row) => {
-    return row.categoryId.title;
   };
 
   render() {
@@ -31,18 +43,17 @@ class BookTable extends Component {
       onSortChange: this.onSortChange
     };
 
-    const book = this.props.book;
-
     return (
       <div>
-        <BootstrapTable data={book} hover pagination options={options} headerStyle={ { background: '#6ab9ff' } }>
+        <BootstrapTable data={this.state.books} hover pagination options={options} headerStyle={ { background: '#6ab9ff' } }>
           <TableHeaderColumn width='100' dataField='inventoryCode' isKey dataSort>Номер</TableHeaderColumn>
+          <TableHeaderColumn dataField='registerDate' dataSort>Дата регистрации</TableHeaderColumn>
           <TableHeaderColumn dataField='author' dataSort>Автор</TableHeaderColumn>
           <TableHeaderColumn dataField='title' dataSort>Название</TableHeaderColumn>
-          <TableHeaderColumn width='120' dataField='year' dataSort>Год издания</TableHeaderColumn>
+          <TableHeaderColumn width='70' dataField='year' dataSort>Год издания</TableHeaderColumn>
           <TableHeaderColumn dataField='publishHouse' dataSort>Издательство</TableHeaderColumn>
-          <TableHeaderColumn dataFormat={this.categoryFormat} dataField='categoryId' dataSort>Категория</TableHeaderColumn>
-          <TableHeaderColumn width='120' dataFormat={this.languageFormat} dataField='language' dataSort>Язык</TableHeaderColumn>
+          <TableHeaderColumn dataField='categoryId' dataSort>Категория</TableHeaderColumn>
+          <TableHeaderColumn width='120' dataField='language' dataSort>Язык</TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
