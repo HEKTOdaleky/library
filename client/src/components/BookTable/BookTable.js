c92b67266dc14b9db36dc9bd403859ff51483fcf
Aleksander Gareev <saver2003@gmail.com>
2018-07-24 20:49:42 +0600

    #91 Добавил селект для выбора категории книг

--- client/src/components/BookTable/BookTable.js
+++ client/src/components/BookTable/BookTable.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import moment from "moment/moment";
+import {getCategories} from "../../store/actions/categories";
+import {connect} from "react-redux";
+import {Button, Form, FormControl, FormGroup, InputGroup, Panel} from "react-bootstrap";
+import FormElement from "../UI/Form/FormElement";
+import {sortArrayOfObjectsByKey} from "../../lib";
+import {getBook} from "../../store/actions/books";
 
 class BookTable extends Component {
 
   state = {
     books: [],
+    categories: {},
     sortName: undefined,
-    sortOrder: undefined
+    sortOrder: undefined,
+    categoryId: ''
+  };
+
+  componentDidMount() {
+    this.props.getCategories();
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.book) {
-
+    if (nextProps.book && nextProps.categories) {
       const books = nextProps.book.map(book => {
         const registerDate = moment(book.registerDate).format("DD-MM-YYYY h:mm");
         return {
@@ -23,16 +34,40 @@ class BookTable extends Component {
         }
       });
 
-      return {books: books};
+      let categories = {};
+
+      nextProps.categories.forEach((category, index) => {
+        categories[index] = category.title;
+      });
+      console.log(categories);
+
+      return {books: books, categories: categories};
     }
 
-    return {books: []};
-  }
+
+    return {...prevState};
+  };
+
+  enumFormatter = (cell, row, enumObject) => {
+    return enumObject[cell];
+  };
+
 
   onSortChange = (sortName, sortOrder) => {
     this.setState({
       sortName,
       sortOrder
+    });
+  };
+
+  categorySubmitHandler = event => {
+    event.preventDefault();
+    this.props.getCategories(this.state);
+  };
+
+  onChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
     });
   };
 
@@ -43,8 +78,25 @@ class BookTable extends Component {
       onSortChange: this.onSortChange
     };
 
+    const categories = this.props.categories.map(category => {
+      return {id: category._id, title: category.title};
+    });
+    categories.unshift({id: '', title:  'Выберите категорию ...'});
+
     return (
       <div>
+        <Form horizontal onSubmit={this.categorySubmitHandler}>
+        <FormElement
+          propertyName="categoryId"
+          title="Категория"
+          type="select"
+          options={categories}
+          value={this.state.categoryId}
+          changeHandler={this.onChangeHandler}
+          error={this.props.postError &&
+          this.props.postError.message}
+        />
+        </Form>
         <BootstrapTable data={this.state.books} hover pagination options={options} headerStyle={ { background: '#6ab9ff' } }>
           <TableHeaderColumn width='100' dataField='inventoryCode' isKey dataSort>Номер</TableHeaderColumn>
           <TableHeaderColumn dataField='registerDate' dataSort>Дата регистрации</TableHeaderColumn>
@@ -52,7 +104,14 @@ class BookTable extends Component {
           <TableHeaderColumn dataField='title' dataSort>Название</TableHeaderColumn>
           <TableHeaderColumn width='70' dataField='year' dataSort>Год издания</TableHeaderColumn>
           <TableHeaderColumn dataField='publishHouse' dataSort>Издательство</TableHeaderColumn>
-          <TableHeaderColumn dataField='categoryId' dataSort>Категория</TableHeaderColumn>
+          <TableHeaderColumn
+            dataField='categoryId'
+            // filterFormatted
+            // dataFormat={ this.enumFormatter }
+            // dataSort
+            // formatExtraData={ this.state.categories }
+            // filter={ { type: 'SelectFilter', options: this.state.categories } }
+          >Категория</TableHeaderColumn>
           <TableHeaderColumn width='120' dataField='language' dataSort>Язык</TableHeaderColumn>
         </BootstrapTable>
       </div>
@@ -60,4 +119,17 @@ class BookTable extends Component {
   }
 }
 
-export default BookTable;
+const mapStateToProps = state => {
+  return {
+    categories: sortArrayOfObjectsByKey(state.categories.categories, 'title')
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getCategories: () => dispatch(getCategories()),
+    getBook: () => dispatch(getBook())
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookTable);
