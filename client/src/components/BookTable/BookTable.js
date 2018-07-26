962e93cb7dd34938a7cd2cce349a389312d2d526
Aleksander Gareev <saver2003@gmail.com>
2018-07-26 20:52:26 +0600

    #91 выборка по категориям и по статусу работают

--- client/src/components/BookTable/BookTable.js
+++ client/src/components/BookTable/BookTable.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
 import moment from "moment/moment";
 import {getCategories} from "../../store/actions/categories";
 import {connect} from "react-redux";
-import {Form} from "react-bootstrap";
+import {Form, Panel} from "react-bootstrap";
 import FormElement from "../UI/Form/FormElement";
 import {sortArrayOfObjectsByKey} from "../../lib";
 import {getBook} from "../../store/actions/books";
@@ -13,11 +14,10 @@ class BookTable extends Component {
 
   state = {
     books: [],
-    categories: {},
-    statusId: {},
     sortName: undefined,
     sortOrder: undefined,
-    categoryId: ''
+    categoryId: '',
+    statusId: ''
   };
 
   componentDidMount() {
@@ -26,27 +26,20 @@ class BookTable extends Component {
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.book && nextProps.categories) {
-      const books = nextProps.book.map(book => {
-        const registerDate = moment(book.registerDate).format("DD-MM-YYYY h:mm");
-        return {
-          ...book,
-          language: book.language.title,
-          categoryId: book.categoryId.title,
-          registerDate
-        }
-      });
+    const books = nextProps.book.filter(book => {
+      return ((book.categoryId._id === prevState.categoryId || prevState.categoryId === '') &&
+        (book.statusId._id === prevState.statusId || prevState.statusId === ''));
+    }).map(book => {
+      const registerDate = moment(book.registerDate).format("DD-MM-YYYY h:mm");
+      return {
+        ...book,
+        language: book.language.title,
+        categoryId: book.categoryId.title,
+        registerDate
+      }
+    });
 
-      let categories = {};
-
-      nextProps.categories.forEach((category, index) => {
-        categories[index] = category.title;
-      });
-
-      return {books: books, categories: categories};
-    }
-
-    return {...prevState};
+    return {books: books};
   };
 
   onSortChange = (sortName, sortOrder) => {
@@ -56,21 +49,12 @@ class BookTable extends Component {
     });
   };
 
-  categorySubmitHandler = event => {
-    event.preventDefault();
-    this.props.getCategories(this.state);
-  };
-
-  statusSubmitHandler = event => {
-    event.preventDefault();
-    this.props.getStatus(this.state)
-  };
-
   onChangeHandler = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
   };
+
 
   render() {
     const options = {
@@ -82,42 +66,53 @@ class BookTable extends Component {
     const categories = this.props.categories.map(category => {
       return {id: category._id, title: category.title};
     });
-    categories.unshift({id: '', title:  'Выберите категорию ...'});
+
+    categories.unshift({id: '', title: 'Выберите категорию ...'});
 
     const status = this.props.status.map(state => {
       return {id: state._id, title: state.name};
     });
-    status.unshift({id: '', title:  'Выберите статус ...'});
+
+    status.unshift({id: '', title: 'Выберите статус ...'});
 
     return (
       <div>
-        <Form horizontal onSubmit={this.categorySubmitHandler}>
-        <FormElement
-          propertyName="categoryId"
-          title="Категория"
-          type="select"
-          options={categories}
-          value={this.state.categoryId}
-          changeHandler={this.onChangeHandler}
-          error={this.props.postError &&
-          this.props.postError.message}
-        />
-        </Form>
+        <Form inline>
+          <Panel>
 
+            <Panel.Heading>Фильтр по выборке</Panel.Heading>
+            <Panel.Body>
 
-        <Form horizontal onSubmit={this.statusSubmitHandler}>
-        <FormElement
-            propertyName="statusId"
-            title="Статус"
-            type="select"
-            options={status}
-            value={this.state.statusId}
-            changeHandler={this.onChangeHandler}
-            error={this.props.postError &&
-            this.props.postError.message}
-          />
+              <FormElement
+                propertyName="categoryId"
+                type="select"
+                options={categories}
+                value={this.state.categoryId}
+                changeHandler={this.onChangeHandler}
+                error={this.props.postError &&
+                this.props.postError.message}
+              />
+
+              <FormElement
+                propertyName="statusId"
+                type="select"
+                options={status}
+                value={this.state.statusId}
+                changeHandler={this.onChangeHandler}
+                error={this.props.postError &&
+                this.props.postError.message}
+              />
+            </Panel.Body>
+          </Panel>
+
         </Form>
-        <BootstrapTable data={this.state.books} hover pagination options={options} headerStyle={ { background: '#6ab9ff' } }>
+        <BootstrapTable
+          data={this.state.books}
+          hover
+          pagination
+          options={options}
+          headerStyle={{background: '#6ab9ff'}}
+        >
           <TableHeaderColumn width='100' dataField='inventoryCode' isKey dataSort>Номер</TableHeaderColumn>
           <TableHeaderColumn dataField='registerDate' dataSort>Дата регистрации</TableHeaderColumn>
           <TableHeaderColumn dataField='author' dataSort>Автор</TableHeaderColumn>
@@ -136,7 +131,6 @@ const mapStateToProps = state => {
   return {
     categories: sortArrayOfObjectsByKey(state.categories.categories, 'title'),
     status: sortArrayOfObjectsByKey(state.status.status, 'name')
-
   }
 };
 
